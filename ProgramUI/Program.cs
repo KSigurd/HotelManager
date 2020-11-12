a30759883239c412d8d67c91dfd558609d256a0d
Kristoffer Sigurd <ksigurd10@gmail.com>
2020-11-12 11:10:46 +0100

    Nu pushar jag

--- ProgramUI/Program.cs
+++ ProgramUI/Program.cs
@@ -6,8 +6,13 @@ namespace ProgramUI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Filip!");
+            Console.WriteLine("Hello Kristoffer!");
+            Console.WriteLine("Hello Kristoffffffeeeeeer!");
+            Console.WriteLine("Hello Kristoffffffeeeeeer!");
+            Console.WriteLine("Hello Linn!");
+            Console.WriteLine("Hello OliBod!");
+            Console.ReadLine();
         }
-        
-
     }
 }
